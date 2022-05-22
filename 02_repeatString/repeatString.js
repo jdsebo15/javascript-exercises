baf36a0da2b422339939e52b5e962ae4411458fc
Jarrett Sebo <Jarrett.Sebo@me.com>
2022-05-22 10:54:08 -0500

    finished problems 1 and 2 of the javascript exercises repo

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let newStr = '';
+    while(num > 0){
+        newStr = newStr + str;
+        num--;
+    }
+    if (num < 0){
+        return "ERROR"
+    } else {
+        return newStr;
+    }
 };
-
+//repeatString('hey',3) for debugging
 // Do not edit below this line
 module.exports = repeatString;
